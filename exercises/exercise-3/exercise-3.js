3a8eb1ec1d9616f91d164fb2a07a99ab96c2b234
fatih-celebi <celebifatih.c1@gmail.com>
2022-11-20 15:44:28 +0000

    Complete Exercise-3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+let totalPrice = 0;
+for (let index = 0; index < order.length; index++) {
+  let sum = order[index]["unitPrice"];
+  totalPrice += sum;
+}
+function createReceipt (order) {
+  console.log(`QTY    ITEM       TOTAL`);
+  order.forEach((item) => {
+    console.log(
+      `${item.quantity}    ${item.itemName}     ${item.unitPrice}`
+    );
+  })
+  console.log(`     Total Price:   ${totalPrice}`);
+}
+
+createReceipt (order);
+
+
